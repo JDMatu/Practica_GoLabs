50ea53f0bccfa399361d3b436278a0f3b32ed5da
MatuDev <juandiegomatute11@gmail.com>
2024-10-15 13:33:15 -0600

    Estructura del proyecto

---
+++ Week2/Proyecto1/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+# Inicializar la aplicación Flask
+app = Flask(__name__)
+
+
+# Definir la ruta principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+# Correr la aplicación
+if __name__ == '__main__':
+    app.run(debug=True)
